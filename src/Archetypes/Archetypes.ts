5a87a1451a6e75b409de33fb7f54329f714b5975
Felipe Fernandes <felipedfe@gmail.com>
2022-08-09 17:09:22 -0300

    requisito 4

---
+++ src/Archetypes/Archetypes.ts
@@ -0,0 +1,41 @@
+// export default abstract class Archetype {
+//   readonly name: string;
+//   readonly special = 0;
+//   readonly cost = 0;
+
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+// }
+
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  private _name: string;
+  private _special: number;
+  private _cost: number;
+
+  constructor(name: string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  abstract get energyType(): EnergyType;
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+}
